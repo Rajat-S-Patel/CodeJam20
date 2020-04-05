ed33ae978fe6597540612e8497d350cdf672f31d
Rajat-S-Patel <53576511+Rajat-S-Patel@users.noreply.github.com>
2020-04-05 09:30:17 +0530

    Add files via upload

--- CODEJAM20_3.cpp
+++ CODEJAM20_3.cpp
@@ -1,38 +1,39 @@
 #include <bits/stdc++.h>
-int main(int argc, char const *argv[])
+using namespace std;
+int main()
 {
 
     int t, k;
-    std::cin >> t;
+    cin >> t;
     for (int k = 0; k < t; k++)
     {
-        std::string s1;
+        string ans;
         int i, n;
-        std::cin >> n;
-        std::vector<std::pair<int, std::pair<int, int>>> v;
+        cin >> n;
+        vector<std::pair<int, std::pair<int, int> > > v;
         int s[n], e[n];
         for (int i = 0; i < n; i++)
         {
 
-            std::cin >> s[i] >> e[i];
+            cin >> s[i] >> e[i];
             v.push_back({s[i], {e[i], i}});
-            s1 += 'C';
+            ans += 'C';
         }
 
-        std::sort(v.begin(), v.end());
+        sort(v.begin(), v.end());
         int c = 0, j = 0, flag = 0;
         for (int i = 0; i < v.size(); i++)
         {
 
             if (v[i].first >= c)
             {
-                s1[v[i].second.second] = 'C';
+                ans[v[i].second.second] = 'C';
                 c = v[i].second.first;
             }
 
             else if (v[i].first >= j)
             {
-                s1[v[i].second.second] = 'J';
+                ans[v[i].second.second] = 'J';
                 j = v[i].second.first;
             }
 
@@ -44,11 +45,11 @@ int main(int argc, char const *argv[])
         }
         if (flag == 1)
         {
-            std::cout << "Case #" << k + 1 << ": "
-                      << "IMPOSSIBLE" << std::endl;
+            cout << "Case #" << k + 1 << ": "
+                      << "IMPOSSIBLE" << endl;
             continue;
         }
-        std::cout << "Case #" << k + 1 << ": " << s1 << std::endl;
+        cout << "Case #" << k + 1 << ": " << ans << endl;
     }
     return 0;
 }
